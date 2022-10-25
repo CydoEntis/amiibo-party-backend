f2f894c08a686f82c8f748bdb3f52b28c80b5c11
Cydo <cydoentis@gmail.com>
2022-10-25 12:01:09 -0400

    moved catch all.

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/amiibos', amiiboRouter);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
